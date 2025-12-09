44216caaa4012defcc15a85459fcdf033e0a853e
SilviaCaraballo <silviacaraballofernandez@gmail.com>
2025-12-09 23:24:58 +0100

    Initial Commit

---
+++ RichNotes/RichNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichNotesApp.swift
+//  RichNotes
+//
+//  Created by Silvia Caraballo Fernandez on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RichNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
